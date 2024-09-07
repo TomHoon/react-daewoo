d8001aac8645b88615c003f48e492b2187027ccb
tomhoon <gnsdl9108@naver.com>
2024-09-07 12:15:43 +0900

    fix: 로그인 튕기기전 alert으로 ux 추가

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,16 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
+    const navigate = useNavigate(); 
+    const goLogin = () => {
+        navigate('/mypage');
+    }
+
     return (
         <div>
             <h3>로그인 페이지</h3>
+            <button onClick={goLogin}>로그인 하기</button>
         </div>
     )
 }
--- src/pages/Mypage.js
+++ src/pages/Mypage.js
@@ -5,6 +5,7 @@ const MyPage = () => {
     const isLogged = false;
 
     if (!isLogged) {
+        alert('로그인 정보가 없습니다.');
         return <Navigate to="/login" replact={true}/>
     } else {
         return <div>마이페이지</div>
