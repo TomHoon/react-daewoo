c3a0fd3110cd1575aa13193f185f885652e27522
tomhoon <gnsdl9108@naver.com>
2024-09-07 11:47:47 +0900

    fix: 반복된 NavLink 컴포넌트화

--- src/pages/Articles.js
+++ src/pages/Articles.js
@@ -1,34 +1,32 @@
 import { NavLink, Outlet } from "react-router-dom"
 
 const Articles = () => {
+
+    {/* note: 'NavLink'는 Active를 받아서 함수로 처리해주는 내장 기능이 있다.. 신기하네 */}
+    return (
+        <div>
+            <Outlet/>
+            <ul>
+                <ArticleItem id={1}/>
+                <ArticleItem id={2}/>
+                <ArticleItem id={3}/>
+            </ul>
+        </div>
+    )
+}
+
+const ArticleItem = ({id}) => {
     const activeStyle = {
         color: 'green',
         fontSize: 21
     };
 
     return (
-        <div>
-            <Outlet/>
-            <ul>
-                <li>
-                    {/* note: 'NavLink'는 Active를 받아서 함수로 처리해주는 내장 기능이 있다.. 신기하네 */}
-                    <NavLink to="/articles/1" style={({isActive}) => (isActive ? activeStyle : undefined)}>
-                        게시글1
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink to="/articles/2" style={({isActive}) => (isActive ? activeStyle : undefined)}>
-                        게시글2
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink to="/articles/3" style={({isActive}) => (isActive ? activeStyle : undefined)}>
-                        게시글3
-                    </NavLink>
-                </li>
-            </ul>
-        </div>
+        <li>
+            <NavLink to={`/articles/${id}`} style={({isActive}) => (isActive ? activeStyle : undefined)}>
+                게시글 {id}
+            </NavLink>
+        </li>
     )
 }
-
 export default Articles;
